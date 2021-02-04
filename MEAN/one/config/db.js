973c6db5d6276c17cf8544d95ca54618929bc8a1
nemilik <jenia.tsip@ya.ru>
2021-02-04 09:26:38 +0300

    client

--- MEAN/one/config/db.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  db: 'mongodb://localhost:27017/web?authSource=admin',
-  secret: 'onemean'
-}
--- widget-master/src/components/modal/Offer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <section>
-    <iframe src="https://egrn.realtycloud.ru/static/offer" width=100% height=1000px frameborder="0" allowfullscreen ></iframe>
-  </section>
-</template>
